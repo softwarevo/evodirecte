3c784ec9877df0467262ab3db9edf1e5193d97ca
Adouche <mr.froid.84@gmail.com>
2026-04-02 19:40:05 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,20 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-const branchName = process.env.VITE_BRANCH_NAME;
+export default defineConfig(({ mode }) => {
+  const branchName = process.env.GITHUB_REF_NAME || process.env.VITE_BRANCH_NAME;
 
-const base = branchName && branchName !== 'main' 
-  ? `/${branchName}/` 
-  : '/';
+  const base = branchName && branchName !== 'main' 
+    ? `/${branchName}/` 
+    : '/';
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: base,
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  return {
+    plugins: [react(), tailwindcss()],
+    base: base,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  }
 })
